f96ec81494e4f166e32c58daddbc27f0c6105e29
20creamy21 <tsuyoshiwaga4310@gmail.com>
2021-05-22 14:37:51 +0000

    [Add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "homes#top"
+  get '/about' => 'homes#about'
+
+  resources :games, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :comments, only: [:new, :create, :index, :destroy]
+
 end
