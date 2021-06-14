4547e095372f8f9295439b992c66bd6483017416
20creamy21 <tsuyoshiwaga4310@gmail.com>
2021-06-14 12:06:19 +0000

    [Fix] ログイン機能修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,12 @@ class RelationshipsController < ApplicationController
     @post = Post.find(params[:post_id])
     @user = @post.user
     following = current_user.follow(params[:follow_id])
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-    end
+    following.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     following = current_user.unfollow(params[:follow_id])
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-    end
+    following.destroy
   end
 end
